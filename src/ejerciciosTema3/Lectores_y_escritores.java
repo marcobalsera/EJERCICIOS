8a2e9b8e5774db548fbe4d4c0e150f446a0e5c19
marcobalsera <marco.balsera@alumnos.upm.es>
2025-11-23 12:15:38 +0100

    Ejercicio tema 3
    Ejercicio del tema 3 con algunos cambios

--- src/ejerciciosTema3/Lectores_y_escritores.java
+++ src/ejerciciosTema3/Lectores_y_escritores.java
@@ -6,90 +6,59 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Lectores_y_escritores {
 
-    // Recurso compartido
-    private static int recurso = 0;
+	public static int valorCompartido = 0;
 
-    // Lock para controlar acceso
-    private static final ReadWriteLock lock = new ReentrantReadWriteLock();
+	public static synchronized int leer() {
+		return valorCompartido;
+	}
 
-    // Clase para hebra lectora
-    static class Lectora implements Runnable {
-        private final int id;
+	public static synchronized void escribir(int nuevoValor) {
+		valorCompartido = nuevoValor;
+	}
 
-        public Lectora(int id) {
-            this.id = id;
-        }
+	static class Lector implements Runnable {
+		private final int id;
 
-        @Override
-        public void run() {
-            lock.readLock().lock();
-            try {
-                System.out.println("Lectora " + id + " lee valor: " + recurso);
-            } finally {
-                lock.readLock().unlock();
-            }
-        }
-    }
+		Lector(int id) {
+			this.id = id;
+		}
 
-    // Clase para hebra escritora
-    static class Escritora implements Runnable {
-        private final int id;
+		@Override
+		public void run() {
+			System.out.println("Lector " + id + " lee: " + leer());
+		}
+	}
 
-        public Escritora(int id) {
-            this.id = id;
-        }
+		
+	static class Escritor implements Runnable {
+			private final int id;
 
-        @Override
-        public void run() {
-            lock.writeLock().lock();
-            try {
-                int nuevoValor = ThreadLocalRandom.current().nextInt(0, 10001);
-                recurso = nuevoValor;
-                System.out.println("Escritora " + id + " escribe valor: " + recurso);
-            } finally {
-                lock.writeLock().unlock();
-            }
-        }
-    }
+			Escritor(int id) {
+				this.id = id;
+			}
 
-    public static void main(String[] args) {
-        // Configuración: número de lectoras y escritoras
-        int numLectoras = 5;
-        int numEscritoras = 3;
+			@Override
+			public void run() {
+				int nuevoValor = (int) (Math.random() * 10001);
+				escribir(nuevoValor);
+				System.out.println("Escritor " + id + " cambia a: " + nuevoValor);
+			}
+		}
 
-        // Si se pasan argumentos, se usan
-        if (args.length == 2) {
-            numLectoras = Integer.parseInt(args[0]);
-            numEscritoras = Integer.parseInt(args[1]);
-        }
 
-        List<Thread> hebras = new ArrayList<>();
+	public static void main(String[] args) {
+		int numLectores = 5;
+		int numEscritores = 3;
 
-        // Crear lectoras
-        for (int i = 1; i <= numLectoras; i++) {
-            hebras.add(new Thread(new Lectora(i)));
-        }
+		
+		for (int i = 0; i < numLectores; i++) {
+			new Thread(new Lector(i)).start();
+		}
+		
+		for(int i = 0; i<numEscritores; i++) {
+			new Thread(new Escritor(i)).start();
+		}
+		
+	}
 
-        // Crear escritoras
-        for (int i = 1; i <= numEscritoras; i++) {
-            hebras.add(new Thread(new Escritora(i)));
-        }
-
-        // Lanzar todas las hebras
-        for (Thread t : hebras) {
-            t.start();
-        }
-
-        // Esperar a que terminen
-        for (Thread t : hebras) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("Programa finalizado.");
-    }
 }
-
