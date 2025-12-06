920eb8708fb6b7b4ec0e150d28d7b224f676d4e1
marcobalsera <marco.balsera@alumnos.upm.es>
2025-12-06 20:20:27 +0100

    HTML
    No sé subir las cosas de HTML, pero hemos dado más cosas de java así que cuando pueda las subo

---
+++ src/html/Htmlaviso.java
@@ -0,0 +1,11 @@
+package html;
+
+public class Htmlaviso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("No sé como subir lo que he hecho de HTML de la segunda parte, por lo que subo esto");
+	}
+
+}
